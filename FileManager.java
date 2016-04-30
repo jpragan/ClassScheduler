1baaa8a287f2c83bd7a9b8cd4f1b0eb341457cca
Malcolm Chaney <chaneym@gmail.com>
2016-04-30 16:47:13 -0700

    Create FileManager.java

---
+++ FileManager.java
@@ -0,0 +1,22 @@
+/**
+ * Created by chane_000 on 4/30/2016.
+ */
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
+
+public class FileManager {
+
+    FileManager() {
+
+    }
+
+    public void write_string(String input_string) {
+        //Files.write(Paths.get("./db.csv"), input_string);
+    }
+
+    public void read_db() {
+        //List<String> lines = Files.readAllLines(Paths.get("./db.csv"));
+    }
+
+}
